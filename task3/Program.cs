eb490136d7a495eff0fe955a10391c379b5fd87a
Mahmudjon03 <138500091+mahmud200405@users.noreply.github.com>
2023-07-26 19:01:13 +0500

    Program.cs

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,17 +1,19 @@
-﻿using Calcullator;
+using Calcullator;
 
 var calculator=new Calculator(4,5);
 
-string n = Console.ReadLine();
-if(n=="+"){
-   System.Console.WriteLine(calculator.Summ()); 
-}else if(n=="-"){
+char n;
+    n = char.Parse(Console.ReadLine());
+if(n=='+'){
+  calculator.Summ(); 
+}else if(n=='-'){
     System.Console.WriteLine(calculator.Subtract());
-}else if(n=="*"){
+}else if(n=='*'){
     System.Console.WriteLine(calculator.Multiplication());
-}else if(n=="/"){
+}else if(n=='/'){
     System.Console.WriteLine(calculator.Division());
 }
+
 
 
 
